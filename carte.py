3125e66e3fe3d8f68e64c4d4270029e19045824d
KOUISAmine <amine.kouis@gmail.com>
2018-07-05 23:25:26 +0100

    Using card to store the session of the player

---
+++ carte.py
@@ -0,0 +1,19 @@
+# -*-coding:Utf-8 -*
+
+"""Ce module contient la classe Carte."""
+
+from labyrinthe import creer_labyrinthe_depuis_chaine
+
+class Carte:
+
+    """Objet de transition entre un fichier et un labyrinthe.
+
+    La carte conserve le nom du fichier. Le labyrinthe est créé depuis le
+    contenu du fichier.
+
+    """
+
+    def __init__(self, nom, chaine):
+        self.nom = nom
+        self.labyrinthe = creer_labyrinthe_depuis_chaine(chaine)
+
