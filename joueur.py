a629cc1d2157a26f39525e3e60b88612a8f7bc16
KOUISAmine <amine.kouis@gmail.com>
2018-07-22 03:22:04 +0100

    Fix Hot Bug On Player Class

--- joueur.py
+++ joueur.py
@@ -16,6 +16,19 @@ from robot import Robot
 class Joueur:
 
     """Classe représentant un joueur.
+
+    Un joueur est un lien entre le Jeu et une connexion réseau.
+    Il est principalement là pour exécuter les commandes
+    entrées. Notez cependant que ce mécanisme est utilisé par le
+    serveur (module 'serveur').
+
+    Pour construire un joueur, vous devez préciser :
+        jeu -- le jeu (voir le module 'jeu' et la classe 'Jeu')
+        client -- le socket connecté
+
+    Un joueur est normalement automatiquement créé par le serveur
+    qui appelle la méthode 'ajouter_joueur' de la classe 'Jeu'.
+
     """
 
     def __init__(self, jeu, client):
@@ -31,7 +44,7 @@ class Joueur:
         """Envoie le message au client (socket)."""
         if self.client:
             self.client.send(message.encode())
-			
+
     def placer_robot(self):
         """Place le robot aléatoirement sur la grille."""
         labyrinthe = self.jeu.labyrinthe
@@ -39,7 +52,7 @@ class Joueur:
         libres = []
         x = 0
         y = 0
-		
+
         # D'abord, on cherche les limites x et y de la grille
         l_x = labyrinthe.limite_x
         while l_x > 0:
@@ -58,15 +71,10 @@ class Joueur:
             while x < l_x:
                 if (x, y) not in grille:
                     libres.append((x, y))
-					libres.append((y, x))
                 x += 1
             y += 1
-            if (0, l_y) in grille:
-                break
-            l_y -= 1
-			
+
         x, y = choice(libres)
         robot = Robot(x, y)
         labyrinthe.ajouter_robot(robot)
         self.robot = robot
-		
