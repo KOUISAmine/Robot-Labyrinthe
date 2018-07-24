fb02b205bf45e75f9a127362cd3e964b06b67237
KOUISAmine <amine.kouis@gmail.com>
2018-07-25 00:06:35 +0100

    # Create the game

--- serveur.py
+++ serveur.py
@@ -1,4 +1,11 @@
+# -*-coding:Utf-8 -*
 
+"""Ce fichier contient le code du serveur.
+
+Il faut exécuter ce fichier avant de lancer un ou plusieurs clients.
+Les clients sont contenus dans le fichier 'client.py'.
+
+"""
 
 import os
 import socket
@@ -8,3 +15,8 @@ from carte import Carte
 from commande import commandes
 from jeu import Jeu
 
+# Create the game
+jeu = Jeu(carte)
+for i, commande in enumerate(commandes):
+    # Commande Instanciation
+    commandes[i] = commande(jeu)
